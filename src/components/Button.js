bcf3a038b0b3d6f23ca1355aa6383790c9ac225e
javascriptsucks <evix1988@live.cn>
2023-02-21 00:40:25 -0600

    built-button-component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,21 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-	return <></>;
+	let buttonClass = "button";
+
+	if (props.confirm) {
+		buttonClass += " button--confirm";
+	}
+	if (props.danger) {
+		buttonClass += " button--danger";
+	}
+
+	return (
+		<button
+			disabled={props.disabled}
+			onClick={props.onClick}
+			className={buttonClass}>
+			{props.children}
+		</button>
+	);
 }
