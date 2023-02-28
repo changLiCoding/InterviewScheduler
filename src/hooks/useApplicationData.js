cec5e5b8a4ad94e7bfa0b56417e76bf60cddf73b
javascriptsucks <evix1988@live.cn>
2023-02-28 00:45:00 -0600

    useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,16 +1,73 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 
 import axios from "axios";
 
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+const DELETE_INTERVIEW = "DELETE_INTERVIEW";
+
+const updateSpots = (id, newState) => {
+	const theDayOfAppointmentsObj = newState.days.find((day) =>
+		day.appointments.includes(id)
+	);
+	console.log(theDayOfAppointmentsObj);
+	const spotsOfTheDay = theDayOfAppointmentsObj.appointments.reduce(
+		(prev, curv) => {
+			if (newState.appointments[curv].interview === null) {
+				prev++;
+			}
+			return prev;
+		},
+		0
+	);
+	theDayOfAppointmentsObj.spots = spotsOfTheDay;
+
+	const days = newState.days.map((day) => {
+		if (day.id === theDayOfAppointmentsObj.id) {
+			return theDayOfAppointmentsObj;
+		} else {
+			return day;
+		}
+	});
+	newState.days = days;
+	return newState;
+};
+
+const reducer = function (state, action) {
+	switch (action.type) {
+		case SET_DAY:
+			console.log(action.day);
+			return { ...state, day: action.day };
+		case SET_APPLICATION_DATA:
+			console.log(action.days, action.interviewers);
+			return {
+				...state,
+				appointments: action.appointments,
+				days: action.days,
+				interviewers: action.interviewers,
+			};
+		case SET_INTERVIEW:
+			return { ...state, ...updateSpots(action.id, { ...action.newState }) };
+		case DELETE_INTERVIEW:
+			return { ...state, ...updateSpots(action.id, { ...action.newState }) };
+
+		default:
+			throw new Error(`
+      Tried to reduce with unsupported action type: ${action.type}
+      `);
+	}
+};
+
 export default function useApplicationData() {
-	const [state, setState] = useState({
+	const [state, dispatch] = useReducer(reducer, {
 		day: "Monday",
 		days: [],
 		appointments: {},
 		interviewers: {},
 	});
 
-	const setDay = (day) => setState({ ...state, day });
+	const setDay = (day) => dispatch({ type: SET_DAY, day });
 
 	useEffect(() => {
 		Promise.all([
@@ -19,43 +76,15 @@ export default function useApplicationData() {
 			axios.get("/api/interviewers"),
 		])
 			.then((res) => {
-				setState((prev) => ({
-					...prev,
+				dispatch({
+					type: SET_APPLICATION_DATA,
 					days: res[0].data,
 					appointments: res[1].data,
 					interviewers: res[2].data,
-				}));
-				console.log(res);
+				});
 			})
 			.catch((err) => console.error(err));
 	}, []);
-
-	const updateSpots = (id, newState) => {
-		const theDayOfAppointmentsObj = newState.days.find((day) =>
-			day.appointments.includes(id)
-		);
-		console.log(theDayOfAppointmentsObj);
-		const spotsOfTheDay = theDayOfAppointmentsObj.appointments.reduce(
-			(prev, curv) => {
-				if (newState.appointments[curv].interview === null) {
-					prev++;
-				}
-				return prev;
-			},
-			0
-		);
-		theDayOfAppointmentsObj.spots = spotsOfTheDay;
-
-		const days = newState.days.map((day) => {
-			if (day.id === theDayOfAppointmentsObj.id) {
-				return theDayOfAppointmentsObj;
-			} else {
-				return day;
-			}
-		});
-		newState.days = days;
-		return newState;
-	};
 
 	const bookInterview = function (id, interview) {
 		return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
@@ -68,13 +97,10 @@ export default function useApplicationData() {
 				...state.appointments,
 				[id]: appointment,
 			};
-			setState((prev) => {
-				let newState = {
-					...prev,
-					appointments,
-				};
-
-				return updateSpots(id, newState);
+			dispatch({
+				type: SET_INTERVIEW,
+				id,
+				newState: { ...state, appointments },
 			});
 		});
 	};
@@ -92,15 +118,19 @@ export default function useApplicationData() {
 					...state.appointments,
 					[id]: appointment,
 				};
-				setState((prev) => {
-					const newstate = {
-						...prev,
-						appointments,
-					};
-					return updateSpots(id, newstate);
+
+				dispatch({
+					type: DELETE_INTERVIEW,
+					id,
+					newState: { ...state, appointments },
 				});
 			});
 	};
 
-	return { setDay, state, cancelInterview, bookInterview };
+	return {
+		setDay,
+		state,
+		cancelInterview,
+		bookInterview,
+	};
 }
