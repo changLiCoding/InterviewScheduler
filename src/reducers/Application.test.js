12660cf5914fd7f0e30b9e476b58049120bbb312
javascriptsucks <evix1988@live.cn>
2023-03-01 13:18:06 -0600

    test-cover-96%

--- src/reducers/Application.test.js
+++ src/reducers/Application.test.js
@@ -3,3 +3,11 @@ import reducer, {
 	SET_APPLICATION_DATA,
 	SET_DAY,
 } from "./Application";
+
+describe("Application Reducer", () => {
+	it("thows an error with an unsupported type", () => {
+		expect(() => reducer({}, { type: null })).toThrowError(
+			/tried to reduce with unsupported action type/i
+		);
+	});
+});
